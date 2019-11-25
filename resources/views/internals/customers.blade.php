49519c8c3a2c1a84e7d500fb1acbdef6eed9eb40
Apple-vCTO <messi.555.fan@gmail.com>
2019-11-25 13:24:59 +1000

    Adding Email For Customers

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -4,12 +4,16 @@
 	<h1>Customers</h1>
 
 	<form action="customers" method="POST" class="pb-5">
-		<div class="input-group">
-			<input type="text" name="name">
+		<p>Name:</p>
+		<div class="input-group pb-2">
+			<input type="text" name="name" value="{{ old('name') }}">
+			<div>{{ $errors->first('name') }}</div>
 		</div>
 
-		<div>
-			{{ $errors->first('name') }}
+		<p>Email:</p>
+		<div class="input-group">
+			<input type="text" name="email" value="{{ old('email') }}">
+			<div>{{ $errors->first('email') }}</div>
 		</div>
 
 		<button type="submit">Add Customer</button>
@@ -19,7 +23,7 @@
 
 	<ul>
 		@foreach ($customers as $customer)
-			<li>{{ $customer->name }}</li>
+			<li>{{ $customer->name }} <span class="text-muted">({{ $customer->email }})</span></li>
 		@endforeach
 	</ul>
 @endsection
