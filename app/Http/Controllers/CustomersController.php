10a963ae4a0f60b23f05c25f478500a83396c8a5
Apple-vCTO <messi.555.fan@gmail.com>
2019-11-25 12:50:55 +1000

    Adding customers to the database

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -15,4 +15,13 @@ class CustomersController extends Controller
 			'customers' =>  $customers
 		]);
     }
+
+    public function store()
+    {
+    	$customer = new Customer();
+    	$customer->name = request('name');
+    	$customer->save();
+
+    	return back();
+    }
 }
